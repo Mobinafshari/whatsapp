62b8b33b07d05b6f167bfab02100d1bad31cd963
Mobinafshari <mma137813781378@gmail.com>
2023-09-05 11:40:57 +0430

    logger added

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import app from "./app.js";
+import logger from "./src/configs/logger.js";
 
 
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT,() =>{
-    console.log(`sever is listening at ${PORT}`)
+    logger.info(`sever is listening at ${PORT}`)
 });
