312bac8b198d8536848c6d5c06d1557386ce8046
Mobinafshari <mma137813781378@gmail.com>
2023-09-05 10:50:39 +0430

    adding middleware

--- app.js
+++ app.js
@@ -1,12 +1,37 @@
+import dotenv from "dotenv";
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import mongoSanitize from 'express-mongo-sanitize';
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import fileUpload from "express-fileupload";
+import cors from 'cors';
 
-
+//dotenv config
+dotenv.config();
 
 //create express app
 const app = express();
 
-app.get("/test",(req,res)=>{
-    res.status(205).send("hello this is a test")
-})
+//use morgan
+if(process.env.NODE_ENV !== "production"){
+    app.use(morgan("dev"));
+}
+//middlewares
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended:true}));
+app.use(mongoSanitize());
+app.use(cookieParser());
+app.use(compression());
+app.use(fileUpload({
+  useTempFiles:true
+}));
+app.use(cors());
+
+app.post("/test", (req, res) => {
+  res.status(200).json(req.body);
+});
 
 export default app;
--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-import dotenv from "dotenv";
 import app from "./app.js";
 
-//dotenv config
-dotenv.config();
+
 
 const PORT = process.env.PORT || 8000;
-
 app.listen(PORT,() =>{
     console.log(`sever is listening at ${PORT}`)
 });
